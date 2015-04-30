94b65110552d4c5bd96273ab060233154adda2d0
제민재 <megajeka@naver.com>
2015-04-30 16:58:20 +0900

    Shape ver2!!!

---
+++ src/Shape.java
@@ -0,0 +1,16 @@
+public abstract class Shape {
+
+	private String color;
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	private String getColor(){
+		return color;
+	}
+	
+	public abstract void draw();
+	public abstract double calculateArea();
+
+}
